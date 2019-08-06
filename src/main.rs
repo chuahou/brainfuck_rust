57657fd2c3c781db798056409250a530873210cd
Chua Hou <human@chuahou.dev>
2019-08-06 17:55:59 +0800

    Add command line options via clap (not yet implemented)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,52 @@
 mod interpreter;
 
+extern crate clap;
+
 fn main()
 {
-    // get configuration
-    let tape_size: usize = 30000;
-    let helloworld = b"++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+    // use clap to get cli arguments
+    let matches = clap::App::new("brainfuck_rust")
+                    .version(clap::crate_version!())
+                    .author(clap::crate_authors!())
+                    .about("Yet yet another brainfuck interpreter, now in Rust")
+                    .arg(clap::Arg::with_name("PROGRAM")
+                        .help("Body of program to interpret")
+                        .index(1))
+                    .arg(clap::Arg::with_name("inputfile")
+                        .short("f")
+                        .long("file")
+                        .value_name("INPUT FILE")
+                        .help("Reads program from file")
+                        .takes_value(true))
+                    .group(clap::ArgGroup::with_name("input").required(true)
+                        .args(&["PROGRAM", "inputfile"]))
+                    .arg(clap::Arg::with_name("tapesize")
+                        .short("t")
+                        .long("tapesize")
+                        .value_name("TAPE SIZE")
+                        .help("Sets size of brainfuck tape")
+                        .takes_value(true))
+                    .get_matches();
+
+    // read program from file or from arguments
     let mut program: Vec<u8> = Vec::new();
+    match matches.value_of("inputfile")
+    {
+        // read from file
+        Some(filename) => (),
+
+        // read from argument
+        None => (),
+    }
+
+    // get tape size
+    let tape_size: usize = match matches.value_of("tapesize")
+    {
+        Some(tape_size) => tape_size.parse::<usize>().unwrap(),
+        None => 30000,
+    };
+
+    let helloworld = b"++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
     for byte in helloworld.iter()
     {
         program.push(*byte);
