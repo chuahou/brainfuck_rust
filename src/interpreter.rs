a8cd6a49da82aae574077c495b696eb09d3b98a4
Chua Hou <human@chuahou.dev>
2019-08-06 16:59:32 +0800

    Move interpreting code to separate function and file

---
+++ src/interpreter.rs
@@ -0,0 +1,96 @@
+use std::io::Read;
+
+pub fn interpret(program: Vec<u8>, tape_size: usize)
+{
+    // create tape and pointers
+    let mut tape = vec![0u8; tape_size];
+    let mut tape_ptr: usize = 0;
+    let mut program_ptr: usize = 0;
+
+    // main loop
+    while program_ptr < program.len()
+    {
+        match program[program_ptr]
+        {
+            // increment data cell
+            b'+' => tape[tape_ptr] += 1,
+
+            // decrement data cell
+            b'-' => tape[tape_ptr] -= 1,
+
+            // increment tape pointer
+            b'>' =>
+            {
+                if tape_ptr < tape_size - 1
+                {
+                    tape_ptr += 1;
+                }
+                else
+                {
+                    panic!("Exceeded tape size");
+                }
+            }
+
+            // decrement tape pointer
+            b'<' =>
+            {
+                if tape_ptr > 0
+                {
+                    tape_ptr -= 1;
+                }
+                else
+                {
+                    panic!("Tape pointer moved left of 0");
+                }
+            }
+
+            // print current data
+            b'.' => print!("{}", tape[tape_ptr] as char),
+
+            // input character to current position on data
+            b',' => tape[tape_ptr] =
+                std::io::stdin().bytes().next().unwrap().unwrap(),
+
+            // handle [
+            b'[' =>
+            {
+                // jump to ]
+                if tape[tape_ptr] == 0
+                {
+                    while program[program_ptr] != b']'
+                    {
+                        program_ptr += 1;
+                    }
+                }
+            }
+
+            // handle ]
+            b']' =>
+            {
+                // jump to the matching [ if not zero
+                if tape[tape_ptr] != 0
+                {
+                    // count represents the number of excess ] we've encountered
+                    // minus the number of [ encountered
+                    let mut count = 1;
+                    while count > 0
+                    {
+                        program_ptr -= 1;
+                        match program[program_ptr] 
+                        {
+                            b'[' => count -= 1,
+                            b']' => count += 1,
+                            _ => (),
+                        }
+                    }
+                }
+            }
+
+            // comment
+            _ => println!("..."),
+        }
+
+        // go to next character
+        program_ptr += 1;
+    }
+}
